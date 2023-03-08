d4286110ff66368d2f102fc26b8a9301d25049e2
Pablo Sarzosa <pablo.sarzosa@epn.edu.ec>
2023-03-08 13:32:18 -0500

    Se agregaron cambios para el login

--- frontend/TechCoreComponents/src/app/app.module.ts
+++ frontend/TechCoreComponents/src/app/app.module.ts
@@ -4,7 +4,7 @@ import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { RegistroComponent } from './registro/registro.component';
+import { RegistroComponent } from './components/registro/registro.component';
 
 @NgModule({
   declarations: [
