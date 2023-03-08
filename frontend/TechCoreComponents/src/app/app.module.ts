d4bef3091d3861ef9ec526da11698b90f6339994
Pablo Sarzosa <pablo.sarzosa@epn.edu.ec>
2023-03-08 11:28:27 -0500

    Se agregaron algunos cambios al app module y al app component para aceptar rutas del router. Se creo el componente registro.

--- frontend/TechCoreComponents/src/app/app.module.ts
+++ frontend/TechCoreComponents/src/app/app.module.ts
@@ -1,16 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RegistroComponent } from './registro/registro.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RegistroComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
