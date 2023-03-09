1068f384a81cb3c8ca4896e3cb987e1d5a287e6d
Pablo Sarzosa <pablo.sarzosa@epn.edu.ec>
2023-03-08 22:43:20 -0500

    Login added

--- frontend/techcomponents/src/app/app-routing.module.ts
+++ frontend/techcomponents/src/app/app-routing.module.ts
@@ -2,9 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { RegistroComponent } from './components/registro/registro.component';
 import { InicioComponent } from './components/inicio/inicio.component';
+import { LoginComponent } from './components/login/login.component';
 
 const routes: Routes = [
   {path: 'registro',component: RegistroComponent},
+  {path: 'login', component:LoginComponent},
+  {path: 'inicio',component: InicioComponent},
   {path: '**',component: InicioComponent},
 ];
 
--- frontend/techcomponents/src/app/app.module.ts
+++ frontend/techcomponents/src/app/app.module.ts
@@ -6,12 +6,14 @@ import { AppComponent } from './app.component';
 import { RegistroComponent } from './components/registro/registro.component';
 import { InicioComponent } from './components/inicio/inicio.component';
 import { FormsModule } from '@angular/forms';
+import { LoginComponent } from './components/login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     RegistroComponent,
-    InicioComponent
+    InicioComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
